757a6b5004abb470df56e94fe6bdf4a12c01aa29
amritsagoo91 <amritsagoo91@gmail.com>
2025-06-04 19:33:33 -0700

    database added

--- index.js
+++ index.js
@@ -1,27 +1,16 @@
 const express = require('express')
+require('dotenv').config();
+
+const mongoose = require('mongoose')
+const Note = require('./models/note')
 const app = express()
+
 //const cors = require('cors')
 //app.use(cors({ origin: 'http://localhost:5173' }))
+
 app.use(express.json())
 app.use(express.static('dist'))
 
-let notes = [
-    {
-        id: "1",
-        content: "HTML is easy",
-        important: true
-    },
-    {
-        id: "2",
-        content: "Browser can execute only JavaScript",
-        important: false
-    },
-    {
-        id: "3",
-        content: "GET and POST are the most important methods of HTTP protocol",
-        important: true
-    }
-]
 
 const generateId = () => {
     const maxId = notes.length > 0
@@ -31,13 +20,13 @@ const generateId = () => {
     return String(maxId + 1)
 }
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World</h1>')
-})
 
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes =>
+        response.json(notes)
+    )
+
 })
 
 app.get('/api/notes/:id', (request, response) => {
---
+++ models/note.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+require('dotenv').config();
+
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URI
+mongoose.connect(url)
+    .then(result => {
+        console.log('Connected to MongoDB')
+    })
+    .catch(error => {
+        console.log('Error connecting to MongoDB', error.message)
+    })
+
+const noteSchema = new mongoose.Schema({
+    content: String,
+    important: Boolean
+})
+
+noteSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = new mongoose.model('Note', noteSchema, 'notes')
